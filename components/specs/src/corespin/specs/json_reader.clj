4f44f39ff6deaeb73cab1dde0cca66122e770c65
Ag Ibragimov <agzam.ibragimov@gmail.com>
2024-04-30 03:18:53 -0500

    spec-tools + improved json-reader spec

--- components/specs/src/corespin/specs/json_reader.clj
+++ components/specs/src/corespin/specs/json_reader.clj
@@ -1,6 +1,8 @@
 (ns corespin.specs.json-reader
   (:require
-   [clojure.spec.alpha :as s]))
+   [clojure.spec.alpha :as s]
+   [clojure.test.check.generators :as gen]
+   [spec-tools.impl :as st]))
 
 (s/def ::industries string?)
 (s/def ::targeted_countries string?)
@@ -25,9 +27,31 @@
                    :indicator/indicator
                    :indicator/type]))
 
-(s/def ::feeds (s/coll-of ::feed))
+(s/def ::feeds (s/coll-of ::feed :kind vector?))
 (s/def ::tags (s/map-of :feed/id :feed/tag))
 (s/def ::indicators (s/map-of :feed/id (s/coll-of ::indicator)))
 
 (s/def ::inv-data
-  (s/keys :req-un [::feeds ::tags ::indicators]))
+  (s/with-gen
+    (s/map-of keyword? any?)
+    (fn []
+      (gen/fmap
+       (fn [feeds]
+         (->>
+          feeds
+          (reduce
+           (fn [acc n]
+             (let [fks (->> (s/form ::feed) st/extract-keys (map st/un-key))
+                   feed (-> n
+                            (select-keys fks)
+                            (assoc :industries (gen/generate (s/gen ::industries))
+                                   :targeted_countries (gen/generate (s/gen ::targeted_countries))))
+                   iks (->> (s/form ::indicator) st/extract-keys (map st/un-key))
+                   indicators (->> n :indicators (map #(select-keys % iks)))]
+               (-> acc
+                   (update :feeds conj feed)
+                   (assoc-in [:tags (:id n)] (:tags n))
+                   (assoc-in [:indicators (:id n)] indicators))))
+           {:feeds [] :tags {} :indicators {}})))
+       (s/gen :feed/feeds)))))
+
--- components/sqlite-store/test/corespin/sqlite_store/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns corespin.sqlite-store.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [corespin.sqlite-store.interface :as sqlite-store]))
-
-(deftest dummy-test
-  (is (= 1 1)))
